d96db3697e19615cceb4fc4f62ba5e2e8120b3fe
Aierken Shalayiding <as4824@rit.edu>
2022-11-05 16:16:43 -0400

    new2

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-im = im2double( imread("IMG_3108.JPG") );
+im = im2double( imread("IMG_3127.JPG") );
 im = imresize(im, [300 500]);
 
 
@@ -14,3 +14,27 @@ level = graythresh(im_g);
 disp(level);
 bi_im = imbinarize(im_gray,level);
 imshowpair(im,bi_im,'montage');
+
+
+%bwboundaries return the bound and number of 
+[Bound,L,n,A] = bwboundaries(bi_im); 
+figure; imshow(bi_im); hold on; 
+
+        for i = 1:n 
+        % if it is greater than zero means it is possibly a dice 
+            if (nnz(A(:,i)) > 0) 
+                DiceBound = Bound{i}; 
+                if 400<=length(DiceBound)
+                
+                plot(DiceBound(:,2),DiceBound(:,1),"cyan","LineWidth",3); % start to plot the diceboun
+                end
+            end   
+        end 
+    
+
+
+
+
+
+
+
