887dfeaba1064bb2c75cb7c4dce2b0deef985e9b
Jack Peng <jackpeng2012@126.com>
2022-11-22 14:46:41 -0500

    adding more

--- vein.m
+++ vein.m
@@ -2,7 +2,8 @@
 % im = im2double( imread("IMG_3108.JPG") );
 
 
- im_in =imread("IMG_3422.JPG"); %input image
+ im_in =imread("IMG_3108.JPG"); %input image
+ im_in = imresize(im_in, [300 500]);
  row = size(im_in,1);%row number of image
  col = size(im_in,2);%col number of image
  
@@ -19,17 +20,32 @@
 %  end
 
     color = im_in;
-     color(rgb ~= 2) = 0; 
+     color(rgb ~= 5) = 0; 
+        
    im_c = rgb2gray(color);
 %     figure,imshow(im_c);
   im_c = imbinarize(im_c);
 %   figure,imshow(im_c);
- se2 = strel("disk",20);
-se3 = strel("disk",20);
+ se2 = strel("disk",2);
+se3 = strel("disk",2);
 
 bi_im = imerode(im_c,se3);
  bi_im = imdilate(im_c,se2);
-figure,imshow(bi_im);
+
+
+% %bwboundaries return the bound and number of 
+% [Bound,L,n,A] = bwboundaries(bi_im); 
+% figure; imshow(bi_im); hold on; 
+% 
+% for i = 1:n 
+% % if it is greater than zero means it is possibly a dice 
+% %     if (nnz(A(:,i)) > 0) 
+%         DiceBound = Bound{i}; 
+% %         if 100<=length(DiceBound)       
+%         plot(DiceBound(:,2),DiceBound(:,1),"cyan","LineWidth",3); % start to plot the diceboun
+% %         end
+% %     end   
+% end 
 
 
 % im_gray = rgb2gray(im);
