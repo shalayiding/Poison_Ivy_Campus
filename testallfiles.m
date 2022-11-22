20f8c6de4be1411c87c5390c647e21260cdf0f34
Shalayiding Aierken <salayidin103@gmail.com>
2022-11-22 16:12:17 -0500

    edge and color extract

--- testallfiles.m
+++ testallfiles.m
@@ -2,8 +2,8 @@ function show_all_files_in_dir( )
  % Find all files that match this regular expression:
  file_list = dir('*.jpg');
  for counter = 1 : length( file_list )
- fn = file_list(counter).name;
- main(fn); % run main function and get graphs.
- fprintf('file %3d = %s\n', counter, fn );
+     fn = file_list(counter).name;
+     pipline(fn); % run main function and get graphs.
+     fprintf('file %3d = %s\n', counter, fn );
  end
 end
