b424c58d66a0779e22a60ec2526ecc7aff6ba449
Jack Peng <jackpeng2012@126.com>
2022-11-22 19:05:03 -0500

    sobel binary image

--- edge_detect.m
+++ edge_detect.m
@@ -1,14 +1,31 @@
 function edgeex = edge(im)
-    % break into different rgb channels 
-    im_r = im(:,:,1);
-    im_g = im(:,:,2);
-    im_b = im(:,:,3);
-    im_gray = rgb2gray(im);
-    
-    
-%    im_g    = imfilter( im_g, fspecial('Gauss', 9, 0.9), 'same', 'repl' );
-    
+%     im_in = im2double(imread("IMG_3108.JPG"));
+    im_in = im2double(imread(im));
+    im_g = im_in(:,:,2);
+    im_gray = rgb2gray(im_in);
+    im_g    = imfilter( im_g, fspecial('Gauss', 9, 0.9), 'same', 'repl' );
 
-    im_result = edge(im_gray,'sobel');
-    edgeex =im_result;
+
+    f_sobel_dIdy    = [ -1 -2 -1 ; 
+                         0  0  0 ; 
+                        +1 +2 +1 ] /4;
+    f_sobel_dIdx    = f_sobel_dIdy.';
+
+    dIdy            = imfilter( im_g, f_sobel_dIdy, 'same', 'repl' );
+    dIdx            = imfilter( im_g, f_sobel_dIdx, 'same', 'repl' );
+    dImag           = sqrt( dIdy.^2  + dIdx.^2 );
+for c = 1:size(dImag,1)
+    for l = 1:size(dImag,2)
+        if dImag(c,l) < 0.05
+            dImag(c,l) = 0;
+        else
+            dImag(c,l) = 1;
+        end
+
+    end
 end
+edgeex = dImag;
+pause(5);
+   
+end
+
